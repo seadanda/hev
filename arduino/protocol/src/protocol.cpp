7c0b901de632decb802a004928a49bee88ea5897
Peter Svihra <peter.svihra@cern.ch>
2020-04-13 00:08:36 +0200

    updated lib location

--- arduino/protocol/src/protocol.cpp
+++ arduino/protocol/src/protocol.cpp
@@ -21,6 +21,11 @@ bool blue_ = false;
 bool green_ = false;
 bool red_ = false;
 
+bool enabled_ = false;
+
+uint32_t lastTime_ = 0;
+
+uint32_t offset_ = 10;
 
 // dirty function to switch one of the LEDs
 void switchLED(int led) {
@@ -76,23 +81,40 @@ void loop() {
         }
         previousState_ = currentState_;
     }
-//    switchLED(LED_BLUE);
-    // counter increase on button press
-//    plSend_.getData()->fsm_state += 1;
-//    comms_.writePayload(plSend_);
+
+    if (enabled_ & (millis() > (lastTime_ + offset_)))
+    {
+        lastTime_ = millis();
+
+        plSend_.getData()->readback_valve_air_in = static_cast<uint8_t>((lastTime_ >> 24) & 0xFF);
+        plSend_.getData()->readback_valve_o2_in  = static_cast<uint8_t>((lastTime_ >> 16) & 0xFF);
+        plSend_.getData()->readback_valve_inhale = static_cast<uint8_t>((lastTime_ >> 8 ) & 0xFF);
+        plSend_.getData()->readback_valve_exhale = static_cast<uint8_t>((lastTime_ >> 0 ) & 0xFF);
+
+        switchLED(LED_BLUE);
+        plSend_.setType(payloadType::payloadData);
+        plSend_.getData()->fsm_state += 1;
+        comms_.writePayload(plSend_);
+    }
 
     // per cycle sender
     comms_.sender();
     // per cycle receiver
     comms_.receiver();
 
-    // per cycle data checker - the received entry is automatically removed from the buffer
     if (comms_.readPayload(plReceive_)) {
 
         switch (plReceive_.getType()) {
             case payloadType::payloadCmd:
-                switchLED(plReceive_.getCmd()->cmdCode);
-                alarm_.alarmCode = plReceive_.getCmd()->cmdCode + 1;
+                if (plReceive_.getCmd()->cmdCode % 2 == 0) {
+                    enabled_ = false;
+                } else {
+                    enabled_ = true;
+                }
+                offset_ = plReceive_.getCmd()->param;
+
+                alarm_.alarmCode = plReceive_.getCmd()->cmdCode;
+                alarm_.param     = millis() & 0xFFFFFFFF;
                 plSend_.setAlarm(&alarm_);
                 comms_.writePayload(plSend_);
                 break;
@@ -102,5 +124,4 @@ void loop() {
 
         plReceive_.setType(payloadType::payloadUnset);
     }
-    delay(50);
 }
