07449d62b636db6f042c54635e5474d7fa65f069
Dónal Murray <donal.murray@cern.ch>
2020-05-02 22:47:56 +0100

    Working full duplex send and receive on the async serial port

--- raspberry-dataserver/CommsLLI.py
+++ raspberry-dataserver/CommsLLI.py
@@ -4,6 +4,7 @@ import asyncio
 import serial_asyncio
 import logging
 import binascii
+from collections import deque
 from struct import error as StructError
 from CommsCommon import PayloadFormat, DataFormat
 from CommsFormat import CommsPacket, CommsACK, CommsNACK
@@ -15,50 +16,40 @@ logging.getLogger().setLevel(logging.DEBUG)
 class CommsLLI:
     def __init__(self, loop, queue):
         super().__init__()
-        self._queue = queue
-        self._dv = asyncio.Event()
-        self._connected = False
+        # IO
         self._loop = loop
         self._reader = None
         self._writer = None
+        self._connected = False
+        # send
+        self._alarms = deque(maxlen = 16)
+        self._commands = deque(maxlen = 16)
+        self._data = deque(maxlen = 16)
+        self._datavalid = asyncio.Event()
+        # receive
+        self._sequence_receive = 0
 
-    async def main(self):
-        self._reader, self._writer = await serial_asyncio.open_serial_connection(url='/dev/ttyACM0', baudrate=115200)
+    async def main(self, device, baudrate):
+        self._reader, self._writer = await serial_asyncio.open_serial_connection(url=device, baudrate=baudrate, timeout = 2, dsrdtr = True)
         self._connected = True
         while self._connected:
-            #sent = self.send()
+            #sender = self.send()
             receiver = self.recv()
             await asyncio.gather(*[receiver], return_exceptions=True)
 
-    async def send(self, packet):
-        #for msg in self._queue:
-        logging.info(f"Sending {packet}")
-        await self._writer.write(packet)
-        logging.info(f"Sent {packet}")
+    #async def sender(self, packet):
+    #    while self._sending:
+    #        self._datavalid.wait()
+    #        if self._writer is not None:
+    #            self.sendQueue(self._alarms  ,  10)
+    #            self.sendQueue(self._commands,  50)
+    #            self.sendQueue(self._data    , 200)
+    #        if self.finishedSending():
+    #            self._datavalid.clear()
 
-    def getPayload(self, packet):
-        payload = None
-        comms_response = None
-        #FIXME quick hack to get the new class method working
-        for i in range(1,7):
-            try:
-                payload = PayloadFormat.fromByteArray(bytes([i]) + packet.byteArray)
-            except StructError:
-                continue
-            else:
-                break
-
-        # prepare ack/nack
-        if payload is None:
-            logging.error(f"Invalid payload: {payload}")
-            comms_response = CommsNACK(packet.address)
-        else:
-            logging.debug(f"Received payload type {payload.getType()} for timestamp {payload.timestamp}")
-            comms_response = CommsACK(packet.address)
-
-        comms_response.setSequenceReceive(packet.sequence)
-
-        return payload, comms_response
+    async def sendPacket(self, packet):
+        logging.info(f"Sending {binascii.hexlify(packet.encode())}")
+        self._writer.write(packet.encode())
 
     async def recv(self):
         while self._connected:
@@ -73,15 +64,12 @@ class CommsLLI:
             data = bytearray()
             for el in rawdata:
                 data.extend(bytes([el]))
-
+            
             packet = CommsPacket(data)
-            if packet.decode():
-                # packet contains data
-                payload, response = self.getPayload(packet)
-                print(binascii.hexlify(response.encode()))
-                logging.debug(f"Received packet {binascii.hexlify(payload)}")
-                await self.send(response.encode())
-            else:
+
+            is_response = packet.decode()
+
+            if is_response:
                 # packet is an ack/nack from previously sent data
                 if packet.acked:
                     logging.debug("Received ACK")
@@ -89,30 +77,34 @@ class CommsLLI:
                 else:
                     logging.debug("Received NACK")
                     #trigger another send, if anything is there
-
-    @property
-    def queue(self):
-        return self._queue
-    
-    @queue.setter
-    def queue(self, packet):
-        self._queue.put(packet)
-        self._dv.set()
+            else:
+                # packet should contain data
+                try:
+                    payload = PayloadFormat.fromByteArray(packet.byteArray)
+                    logging.debug(f"Received payload type {payload.getType()} for timestamp {payload.timestamp}")
+                    comms_response = CommsACK(packet.address)
+                except (StructError, ValueError):
+                    # invalid payload
+                    logging.error(f"Invalid payload: {payload}")
+                    comms_response = CommsNACK(packet.address)
+                finally:
+                    comms_response.setSequenceReceive(packet.sequence_receive)
+                    await self.sendPacket(comms_response)
 
 
 if __name__ == "__main__":
-    # setup sender and receiver
-    read_queue = asyncio.Queue()
-    write_queue = asyncio.Queue()
-
-    # schedule async tasks
-    loop = asyncio.get_event_loop()
-
-    # setup serial devices
-    comms = CommsLLI(loop, read_queue)
-
     try:
-        asyncio.gather(comms.main(), return_exceptions=True)
+        # setup sender and receiver
+        read_queue = asyncio.Queue()
+        write_queue = asyncio.Queue()
+
+        # schedule async tasks
+        loop = asyncio.get_event_loop()
+
+        # setup serial devices
+        comms = CommsLLI(loop, read_queue)
+
+        asyncio.gather(comms.main("/dev/ttyUSB0", 115200), return_exceptions=True)
         loop.run_forever()
     except asyncio.CancelledError:
         pass
