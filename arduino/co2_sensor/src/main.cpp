dd7d67cf20b02abd3e03aab228465c45f9cee05a
Karol Hennessy <revkarol@gmail.com>
2020-05-24 13:10:39 +0200

    CO2 sensor example for esp32

---
+++ arduino/co2_sensor/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+HardwareSerial co2_serial(2); // RX, TX
+
+unsigned char hexdata[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}; //Read the gas density command /Don't change the order
+void setup()
+{
+
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        delay(1);
+    }
+    co2_serial.begin(9600);
+}
+
+void loop()
+{
+    co2_serial.write(hexdata, 9);
+    delay(500);
+
+    for (int i = 0, j = 0; i < 9; i++) {
+        if (co2_serial.available() > 0) {
+            long hi, lo, CO2;
+            int ch = co2_serial.read();
+
+            if (i == 2) {
+                hi = ch;
+            } //High concentration
+            if (i == 3) {
+                lo = ch;
+            } //Low concentration
+            if (i == 8) {
+                CO2 = hi * 256 + lo; //CO2 concentration
+                Serial.print("CO2 concentration: ");
+                Serial.print(CO2);
+                Serial.println("ppm");
+            }
+        }
+    }
+}
