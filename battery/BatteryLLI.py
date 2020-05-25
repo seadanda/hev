4183d29b56591c56d05dd970796c5610f2229edc
Peter Svihra <peter.svihra@cern.ch>
2020-05-25 16:34:55 +0200

    error log update

--- battery/BatteryLLI.py
+++ battery/BatteryLLI.py
@@ -5,7 +5,7 @@ import logging
 try:
     import RPi.GPIO as gpio
 except ModuleNotFoundError:
-    logging.error("RPi gpio backend not found")
+    logging.error("RPi gpio backend not found, will use dummy")
     import hevgpio as gpio
 import copy
 from CommsCommon import BatteryFormat
