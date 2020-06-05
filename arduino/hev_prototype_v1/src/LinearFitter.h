bbbb243aacb0275d16004264c6b7c3005b6bb0eb
Peter Svihra <peter.svihra@cern.ch>
2020-06-05 13:43:48 +0200

    fixes of class

--- arduino/hev_prototype_v1/src/LinearFitter.h
+++ arduino/hev_prototype_v1/src/LinearFitter.h
@@ -10,21 +10,67 @@
 
 class LinearFitter
 {
+private:
+    void resetSums() { _entries = 0; _sum_x  = 0; _sum_x2 = 0; _sum_y  = 0; _sum_xy = 0; }
+    void calculateSums(uint32_t &x, float &y, int8_t sign = 1) {
+        _sum_x  += sign * (x         );
+        _sum_x2 += sign * (x * x     );
+        _sum_y  += sign * (        y );
+        _sum_xy += sign * (x     * y );
+    }
+    void calculateSums() {
+        resetSums();
+        for (uint8_t idx = 0; idx < _x.size(); idx++) {
+            if (_x[idx] - _x[0] >= _duration) {
+                break;
+            }
+            _entries++;
+            calculateSums(_x[idx], _y[idx]);
+        }
+    }
+
 public:
-    LinearFitter(uint32_t duration, uint32_t delay);
-
-    void appendPoints(uint32_t x, float y);
-    bool linearRegression(float &slope, float &offset);
-    float extrapolate(uint32_t x);
-
-    void resetCalculation(uint32_t reset_time = 0);
+    LinearFitter(uint32_t duration, uint32_t delay) : _duration(duration), _delay(delay) {;}
     void setDuration(uint32_t duration) { _duration = duration; calculateSums();}
     void setDelay   (uint32_t delay   ) { _delay    = delay   ; calculateSums();}
 
-private:
-    void resetSums();
-    void calculateSums();
-    void calculateSums(uint32_t &x, float &y, int8_t sign = 1);
+    void resetCalculation(uint32_t reset_time = 0) { _x_zero = reset_time; _x.clear(); _y.clear(); }
+
+    void appendPoints(uint32_t x, float y) {
+        x -= _x_zero;
+
+        // remove entry if buffer full or if _duration (in ms) longer than defined
+        while ((!_x.isEmpty() && ((x - _x[0]) >= (_duration + _delay)) ) || (_x.isFull() && _y.isFull())) {
+            uint32_t x_tmp;
+            float    y_tmp;
+            _x.pop(x_tmp);
+            _y.pop(y_tmp);
+        }
+
+        _x.push(x);
+        _y.push(y);
+        calculateSums();
+    }
+
+    bool linearRegression(float &slope, float &offset) {
+        if (_x.isEmpty() || _y.isEmpty())               return false;
+        if ((_x[_entries - 1] - _x[0]) < _duration )    return false;
+        if ((_entries * _sum_x2) == (_sum_x * _sum_x))  return false;
+
+        // results
+        slope  = ((_entries * _sum_xy) - (_sum_x * _sum_y)) / ((_entries * _sum_x2) - (_sum_x * _sum_x));
+        offset = (_sum_y - (slope * _sum_x)) / _entries;
+        return true;
+    }
+    float extrapolate(uint32_t x) {
+        x -= _x_zero;
+        float slope, offset;
+        if (linearRegression(slope, offset)) {
+            return (x*slope + offset);
+        }
+
+        return std::numeric_limits<float>::max(); // max float value
+    }
 
 private:
     uint32_t _x_zero   = 0;
@@ -41,83 +87,5 @@ private:
     float _sum_y  = 0;
     float _sum_xy = 0;
 };
-
-
-LinearFitter::LinearFitter(uint32_t duration, uint32_t delay):
-    _duration(duration),
-    _delay(delay)
-{
-    ;
-}
-
-void LinearFitter::resetCalculation(uint32_t reset_time) {
-    _x_zero = reset_time;
-    _x.clear();
-    _y.clear();
-}
-
-void LinearFitter::resetSums() {
-    _entries = 0;
-
-    _sum_x  = 0;
-    _sum_x2 = 0;
-    _sum_y  = 0;
-    _sum_xy = 0;
-}
-
-void LinearFitter::calculateSums() {
-    resetSums();
-    for (uint8_t idx = 0; idx < _x.size(); idx++) {
-        if (_x[idx] - _x[0] >= _duration) {
-            break;
-        }
-        _entries++;
-        calculateSums(_x[idx], _y[idx]);
-    }
-}
-
-void LinearFitter::calculateSums(uint32_t &x, float &y, int8_t sign) {
-    _sum_x  += sign * (x         );
-    _sum_x2 += sign * (x * x     );
-    _sum_y  += sign * (        y );
-    _sum_xy += sign * (x     * y );
-}
-
-void LinearFitter::appendPoints(uint32_t x, float y) {
-    x -= _x_zero;
-
-    // remove entry if buffer full or if _duration (in ms) longer than defined
-    while ((!_x.isEmpty() && ((x - _x[0]) >= (_duration + _delay)) ) || (_x.isFull() && _y.isFull())) {
-        uint32_t x_tmp;
-        float    y_tmp;
-        _x.pop(x_tmp);
-        _y.pop(y_tmp);
-    }
-
-    _x.push(x);
-    _y.push(y);
-    calculateSums();
-}
-
-bool LinearFitter::linearRegression(float &slope, float &offset) {
-    if (_x.isEmpty() || _y.isEmpty())               return false;
-    if ((_x[_entries - 1] - _x[0]) < _duration )    return false;
-    if ((_entries * _sum_x2) == (_sum_x * _sum_x))  return false;
-
-    // results
-    slope  = ((_entries * _sum_xy) - (_sum_x * _sum_y)) / ((_entries * _sum_x2) - (_sum_x * _sum_x));
-    offset = (_sum_y - (slope * _sum_x)) / _entries;
-    return true;
-}
-
-float LinearFitter::extrapolate(uint32_t x) {
-    x -= _x_zero;
-    float slope, offset;
-    if (linearRegression(slope, offset)) {
-        return (x*slope + offset);
-    }
-
-    return std::numeric_limits<float>::max(); // max float value
-}
 
 #endif // LINEARFITTER_H
