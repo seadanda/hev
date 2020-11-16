9066f6e54b1d4ea58c58e9a45d9649c61eeaf9a7
Karol Hennessy <revkarol@gmail.com>
2020-11-16 14:43:23 +0100

    fix after licence was added

--- ansible/playbooks/hev-display.sh
+++ ansible/playbooks/hev-display.sh
@@ -21,4 +21,5 @@
 # of all those involved with the High Energy Ventilator project
 # (https://hev.web.cern.ch/).
 
+wget -qO - https://hev-sw.gitlab.io/public.key | sudo apt-key add - && echo deb https://hev-sw.gitlab.io/wip/deb/raspbian/ buster main | sudo tee /etc/apt/sources.list.d/hev-sw.list
 
