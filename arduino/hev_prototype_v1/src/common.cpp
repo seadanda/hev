001b25df47e8614cb3954a6437730e2c908f5cac
Derick <derick.sivakumaran@gmail.com>
2021-01-26 16:33:35 +0100

    adjusted ADC2 calibration for O2 sensor

--- arduino/hev_prototype_v1/src/common.cpp
+++ arduino/hev_prototype_v1/src/common.cpp
@@ -243,8 +243,10 @@ float adcToO2PercentFloat(float adc, float offset)
     float PCB_Gain		= 4.		; // real voltage is 4 times higher thant the measured in the PCB (there is a voltage divider)
     float Sensor_Gain		= 100./10000.	; // the sensor gain is 100 % / 10000 mVolts
     float ADC_to_Voltage_Gain	= 3300./4096.0  ; // maximum Voltage of 3.3V for 4096 ADC counts - (It might need recalibration?)
-    
-    float o2pc = PCB_Gain * Sensor_Gain * ADC_to_Voltage_Gain * (adc - offset); 
+    float ADC_to_mVoltage_Gain	= 0.788; 	// this is the measured gain
+    float ADC_offset		= 162.;		// this is the measured offset
+
+    float o2pc = PCB_Gain * Sensor_Gain * (ADC_to_mVoltage_Gain * adc - ADC_offset); 
 
     return o2pc;
 }
