2e057ab8dc0b5212ff711759547598ea93d7e1ee
Benjamin Mummery <benjamin.mummery@stfc.ac.uk>
2021-01-27 12:11:28 +0000

    WIP measurement widgets and new hev_main layout. Also added placeholders for page and start/stop/standby buttons

--- NativeUI/hev_main.py
+++ NativeUI/hev_main.py
@@ -1,17 +1,41 @@
-import logging
 import argparse
+import logging
 import sys
-from PySide2.QtCore import Slot
-from PySide2.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout
+
 from hevclient import HEVClient
+from PySide2.QtCore import Slot
+from PySide2.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QWidget
+from tab_measurements import TabMeasurements
+from tab_page_buttons import TabPageButtons
 from tab_plots import TabPlots
+from tab_start_stop_standby_buttons import TabStartStopStandbyButtons
+
 
 class MainView(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainView, self).__init__(*args, **kwargs)
         hlayout = QHBoxLayout()
-        vlayout = QVBoxLayout()
+        left_vlayout = QVBoxLayout()
+        center_vlayout = QVBoxLayout()
+        right_vlayout = QVBoxLayout()
+
+        # Set up the widget tabs
         self.tab_plots = TabPlots()
-        vlayout.addWidget(self.tab_plots)
-        hlayout.addLayout(vlayout)
+        self.page_buttons = TabPageButtons()
+        self.start_stop_standby_buttons = TabStartStopStandbyButtons()
+        self.measurements = TabMeasurements()
+
+        # left column - page buttons and start/stop/standby
+        left_vlayout.addWidget(self.page_buttons)
+        left_vlayout.addWidget(self.start_stop_standby_buttons)
+        hlayout.addLayout(left_vlayout)
+
+        # center column - plots
+        center_vlayout.addWidget(self.tab_plots)
+        hlayout.addLayout(center_vlayout)
+
+        # right column - measurements
+        right_vlayout.addWidget(self.measurements)
+        hlayout.addLayout(right_vlayout)
+
         self.setLayout(hlayout)
