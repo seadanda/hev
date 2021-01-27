0fef9d6292a28c75bdba45cba02680f12d6da845
Tiago <tiago.sarmento@stfc.ac.uk>
2021-01-27 13:15:36 +0000

    fully merged

--- NativeUI/hev_main.py
+++ NativeUI/hev_main.py
@@ -3,38 +3,35 @@ import logging
 import sys
 
 from hevclient import HEVClient
-<<<<<<< HEAD
 from main_widgets.tab_plots import TabPlots
 from main_widgets.tab_buttons import TabButtons
 from main_widgets.tab_spin import TabSpin
 from main_widgets.tab_valueLabels import TabLabels
 from main_widgets.customButton import customButton, spinRow
 from main_widgets.alarmPopup import alarmPopup
-=======
 from PySide2.QtCore import Slot
 from PySide2.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QWidget
 from tab_measurements import TabMeasurements
 from tab_page_buttons import TabPageButtons
-from tab_plots import TabPlots
 from tab_start_stop_standby_buttons import TabStartStopStandbyButtons
->>>>>>> origin/feature/UI_Measurements
 
 
 class MainView(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainView, self).__init__(*args, **kwargs)
+
         hlayout = QHBoxLayout()
-        self.buttons = TabButtons()
-        hlayout.addWidget(self.buttons)
-        vlayout = QVBoxLayout()
-        self.tab_plots = TabPlots()
-        vlayout.addWidget(self.tab_plots)
-        self.tab_spin = spinRow(self)
-        vlayout.addWidget(self.tab_spin)
-        hlayout.addLayout(vlayout)
-        self.tab_label = TabLabels()
-        hlayout.addWidget(self.tab_label)
-        self.setLayout(hlayout)
+        # self.buttons = TabButtons()
+        # hlayout.addWidget(self.buttons)
+        # vlayout = QVBoxLayout()
+        # self.tab_plots = TabPlots()
+        # vlayout.addWidget(self.tab_plots)
+        # self.tab_spin = spinRow(self)
+        # vlayout.addWidget(self.tab_spin)
+        # hlayout.addLayout(vlayout)
+        # self.tab_label = TabLabels()
+        # hlayout.addWidget(self.tab_label)
+        # #self.setLayout(hlayout)
 
         self.alarmHandler = alarmPopup(self)
         self.alarmHandler.show()
@@ -56,6 +53,8 @@ class MainView(QWidget):
 
         # center column - plots
         center_vlayout.addWidget(self.tab_plots)
+        self.tab_spin = spinRow(self)
+        center_vlayout.addWidget(self.tab_spin)
         hlayout.addLayout(center_vlayout)
 
         # right column - measurements
