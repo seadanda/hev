a7cd59d084232d8658d7fa15b59966ec9dc4939b
Tiago <tiago.sarmento@stfc.ac.uk>
2021-02-09 13:07:54 +0000

    applied template

--- NativeUI/alarm_widgets/tab_clinical.py
+++ NativeUI/alarm_widgets/tab_clinical.py
@@ -1,15 +1,15 @@
 from PySide2 import QtWidgets, QtGui, QtCore
 from settings_widgets.tab_expert import simpleSpin
-
+from global_widgets.template_set_values import TemplateSetValues
 
 class TabClinical(
-    QtWidgets.QWidget
+    TemplateSetValues
 ):  # chose QWidget over QDialog family because easier to modify
     def __init__(self, *args, **kwargs):
         super(TabClinical, self).__init__(*args, **kwargs)
         self.liveUpdating = True
         self.modifications = []
-        self.spinDict = {}
+        #self.spinDict = {}
         clinicalList = [
             ["APNEA", "ms", "duration_calibration"],
             ["Check Pressure Patient", "ms", "duration_buff_purge"],
@@ -25,29 +25,34 @@ class TabClinical(
             ["High PEEP", "", ""],
             ["Low PEEP", "", ""],
         ]
-        grid = QtWidgets.QGridLayout()
-        i = 0
-        for info in clinicalList:
-            self.spinDict[info[0]] = simpleSpin(info)
-            grid.addWidget(self.spinDict[info[0]], int(i / 2), i % 2)
-            i = i + 1
+        self.addSpinDblCol(clinicalList)
+        self.addButtons()
+        self.finaliseLayout()
 
-        hlayout = QtWidgets.QHBoxLayout()
-        self.okButton = QtWidgets.QPushButton()
-        self.okButton.setStyleSheet(
-            "height:50px; background-color:white; border-radius:4px;"
-        )
-        # self.okButton.pressed.connect(self.okButtonPressed)
-        hlayout.addWidget(self.okButton)
 
-        self.cancelButton = QtWidgets.QPushButton()
-        self.cancelButton.setStyleSheet(
-            "height:50px; background-color:white; border-radius:4px;"
-        )
-        # self.cancelButton.pressed.connect(self.cancelButtonPressed)
-        hlayout.addWidget(self.cancelButton)
+        # grid = QtWidgets.QGridLayout()
+        # i = 0
+        # for info in clinicalList:
+        #     self.spinDict[info[0]] = simpleSpin(info)
+        #     grid.addWidget(self.spinDict[info[0]], int(i / 2), i % 2)
+        #     i = i + 1
 
-        vlayout = QtWidgets.QVBoxLayout()
-        vlayout.addLayout(grid)
-        vlayout.addLayout(hlayout)
-        self.setLayout(vlayout)
+        # hlayout = QtWidgets.QHBoxLayout()
+        # self.okButton = QtWidgets.QPushButton()
+        # self.okButton.setStyleSheet(
+        #     "height:50px; background-color:white; border-radius:4px;"
+        # )
+        # # self.okButton.pressed.connect(self.okButtonPressed)
+        # hlayout.addWidget(self.okButton)
+
+        # self.cancelButton = QtWidgets.QPushButton()
+        # self.cancelButton.setStyleSheet(
+        #     "height:50px; background-color:white; border-radius:4px;"
+        # )
+        # # self.cancelButton.pressed.connect(self.cancelButtonPressed)
+        # hlayout.addWidget(self.cancelButton)
+
+        # vlayout = QtWidgets.QVBoxLayout()
+        # vlayout.addLayout(grid)
+        # vlayout.addLayout(hlayout)
+        # self.setLayout(vlayout)
--- NativeUI/settings_widgets/tab_expert.py
+++ NativeUI/settings_widgets/tab_expert.py
@@ -2,11 +2,11 @@ from PySide2 import QtWidgets, QtGui, QtCore
 from global_widgets.global_spinbox import simpleSpin
 from global_widgets.global_select_button import selectorButton
 from global_widgets.global_send_popup import SetConfirmPopup
-
+from global_widgets.template_set_values import TemplateSetValues
 
 class TabExpert(
-    QtWidgets.QWidget
-):  # chose QWidget over QDialog family because easier to modify
+    TemplateSetValues
+): 
     def __init__(self, *args, **kwargs):
         super(TabExpert, self).__init__(*args, **kwargs)
         self.liveUpdating = True
@@ -45,65 +45,12 @@ class TabExpert(
             ],
         }
 
-        grid = QtWidgets.QGridLayout()
-        self.spinDict = {}
-        i = 0
-        for section in controlDict.keys():
+        self.addExpertControls(controlDict)
+        self.addButtons()
+        self.finaliseLayout()
 
-            self.titleLabel = QtWidgets.QLabel(section)
-            self.titleLabel.setStyleSheet("background-color:white")
-            self.titleLabel.setAlignment(QtCore.Qt.AlignCenter)
-            grid.addWidget(self.titleLabel, i, 0, 1, 6)
-            j = -1
-            for boxInfo in controlDict[section]:
-                j = j + 1
-                # label, units = boxInfo, controlDict[section][boxInfo]
-                self.spinDict[boxInfo[0]] = simpleSpin(boxInfo)
-                grid.addWidget(
-                    self.spinDict[boxInfo[0]], i + 1 + int(j / 3), 2 * (j % 3), 1, 2
-                )
+        # self.timer = QtCore.QTimer()
+        # self.timer.setInterval(160)  # just faster than 60Hz
+        # self.timer.timeout.connect(self.update_settings_data)
+        # self.timer.start()
 
-            i = i + 1 + int(j / 3) + 1
-
-        self.okButton = QtWidgets.QPushButton()
-        self.okButton.setStyleSheet(
-            "height:50px; background-color:white; border-radius:4px;"
-        )
-        self.okButton.pressed.connect(self.okButtonPressed)
-        grid.addWidget(self.okButton, i, 0, 1, 3)
-
-        self.cancelButton = QtWidgets.QPushButton()
-        self.cancelButton.setStyleSheet(
-            "height:50px; background-color:white; border-radius:4px;"
-        )
-        self.cancelButton.pressed.connect(self.cancelButtonPressed)
-        grid.addWidget(self.cancelButton, i, 3, 1, 3)
-
-        self.setLayout(grid)
-
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(160)  # just faster than 60Hz
-        self.timer.timeout.connect(self.update_settings_data)
-        self.timer.start()
-
-    def update_settings_data(self):
-        if self.liveUpdating:
-            for spinBox in self.spinDict:
-                self.spinDict[spinBox].update_readback_value()
-
-    def okButtonPressed(self):
-        message = []
-        self.liveUpdating = True
-        for widget in self.spinDict:
-            # print(widget)
-            if self.spinDict[widget].manuallyUpdated:
-                setVal = self.spinDict[widget].simpleSpin.value()
-                # print('manually updated')
-                print("set" + widget + " to " + str(setVal))
-                message.append(["set" + widget + " to " + str(setVal)])
-                self.spinDict[widget].manuallyUpdated = False
-        self.popup = SetConfirmPopup(message)
-        self.popup.show()
-
-    def cancelButtonPressed(self):
-        self.liveUpdating = True
