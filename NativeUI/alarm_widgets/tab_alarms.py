9803cdfd514e968994ba799578deea18787dd9e7
Benjamin Mummery <benjamin.mummery@stfc.ac.uk>
2021-04-12 10:02:08 +0100

    Corrected bug where tab_alarms.update_alarms was checking for empty lists but not empty dicts

--- NativeUI/alarm_widgets/tab_alarms.py
+++ NativeUI/alarm_widgets/tab_alarms.py
@@ -16,12 +16,16 @@ import sys
 
 from alarm_widgets.alarm_popup import alarmPopup, abstractAlarm
 from alarm_widgets.alarm_list import alarmList
-from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2 import QtWidgets
 
 
 class TabAlarm(QtWidgets.QWidget):
+    """
+    Placeholder
+    """
+
     def __init__(self, NativeUI, *args, **kwargs):
-        super(TabAlarm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.NativeUI = NativeUI
 
         # self.alarmDict = {}
@@ -45,14 +49,21 @@ class TabAlarm(QtWidgets.QWidget):
         # self.timer.timeout.connect(self.updateAlarms)
         # self.timer.start()
 
-    def acknowledge_pressed(self):
+    def acknowledge_pressed(self) -> int:
+        """
+        Placeholder
+        """
         self.popup.clearAlarms()
         self.list.acknowledge_all()
+        return 0
 
     def update_alarms(self):
+        """
+        Placeholder
+        """
         newAlarmPayload = self.NativeUI.get_db("alarms")
-        if newAlarmPayload == []:
-            return
+        if newAlarmPayload == {}:
+            return 1
         if newAlarmPayload["alarm_code"] in self.alarmDict:
             a = 1
             self.alarmDict[newAlarmPayload["alarm_code"]].resetTimer()
@@ -61,15 +72,20 @@ class TabAlarm(QtWidgets.QWidget):
             newAbstractAlarm = abstractAlarm(self.NativeUI, newAlarmPayload)
             self.alarmDict[newAlarmPayload["alarm_code"]] = newAbstractAlarm
             newAbstractAlarm.alarmExpired.connect(
-                lambda i=newAbstractAlarm: self.handleAlarmExpiry(i)
+                lambda i=newAbstractAlarm: self.handle_alarm_expiry(i)
             )
             self.popup.addAlarm(newAbstractAlarm)
             self.list.addAlarm(newAbstractAlarm)
             self.NativeUI.widgets.alarm_table_tab.table.addAlarmRow(newAbstractAlarm)
+        return 0
 
-    def handleAlarmExpiry(self, abstractAlarm):
-        abstractAlarm.freezeTimer()
-        self.popup.removeAlarm(abstractAlarm)
-        self.list.removeAlarm(abstractAlarm)
-        self.alarmDict.pop(abstractAlarm.alarmPayload["alarm_code"])
-        abstractAlarm.recordFinishTime()
+    def handle_alarm_expiry(self, abstract_alarm):
+        """
+        Placeholder
+        """
+        abstract_alarm.freezeTimer()
+        self.popup.removeAlarm(abstract_alarm)
+        self.list.removeAlarm(abstract_alarm)
+        self.alarmDict.pop(abstract_alarm.alarmPayload["alarm_code"])
+        abstract_alarm.recordFinishTime()
+        return 0
